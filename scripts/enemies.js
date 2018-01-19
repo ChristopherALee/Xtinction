3f3d178f6caf36e99b595fd254f9f65c207142da
ChristopherALee <christopheradamlee@gmail.com>
2018-01-19 10:48:12 -0500

    Adjusted spawn rate

--- scripts/enemies.js
+++ scripts/enemies.js
@@ -81,7 +81,7 @@ export const balrogLeft = {
 
 export const spawnRightMonsters= (gameTime, canvas, monsters) => {
   // continuous monster spawn that gradually increases rate of spawn
-  if (Math.random() < 1 - Math.pow(0.99, gameTime / 10)) {
+  if (Math.random() < 1 - Math.pow(0.99, gameTime / 20)) {
     // spawn balrog from right
     monsters.push({
       pos: [canvas.width, Math.random() * (canvas.height - 178)],
@@ -112,7 +112,7 @@ export const spawnRightMonsters= (gameTime, canvas, monsters) => {
     });
   }
 
-  if (Math.random() < 1 - Math.pow(0.99, gameTime / 15)) {
+  if (Math.random() < 1 - Math.pow(0.99, gameTime / 40)) {
     // spawn taurospear from right
     monsters.push({
       pos: [canvas.width, Math.random() * (canvas.height - 175)],
@@ -130,7 +130,7 @@ export const spawnRightMonsters= (gameTime, canvas, monsters) => {
 };
 
 export const spawnLeftMonsters = (gameTime, canvas, monsters) => {
-  if (Math.random() < 1 - Math.pow(0.99, gameTime / 10)) {
+  if (Math.random() < 1 - Math.pow(0.99, gameTime / 20)) {
     // spawn balrog from left
     monsters.push({
       pos: [0 - 178, Math.random() * (canvas.height - 178)],
@@ -144,6 +144,36 @@ export const spawnLeftMonsters = (gameTime, canvas, monsters) => {
         [0, 1, 2, 3]
       ),
       speed: 200
+    });
+
+    // spawn dragonturtle from left
+    monsters.push({
+      pos: [0 - 140, Math.random() * (700 - 130)],
+      sprite: new Sprite(
+        'https://i.imgur.com/a3LCa6u.png',
+        [1100, 125],
+        [140, 130],
+        [0, 0],
+        [150, 150],
+        3,
+        [5, 4, 3, 2, 1, 0]),
+      speed: 100
+    });
+  }
+
+  if (Math.random() < 1 - Math.pow(0.99, gameTime / 40)) {
+    // spawn taurospear from left
+    monsters.push({
+      pos: [0 - 215, Math.random() * (700 - 175)],
+      sprite: new Sprite(
+        'https://i.imgur.com/6TPUotY.png',
+        [2175, 175],
+        [215, 175],
+        [0, 0],
+        [190, 190],
+        6,
+        [2, 1, 0]),
+      speed: 400
     });
   }
 };
